254be18e4d859f6728120a079eb94ea81ee98cb1
Shoaibnadeem <nadeem.naddu7@gmail.com>
2023-03-17 18:01:42 +0530

    initial commit

--- report.sh
+++ report.sh
@@ -1,4 +1,4 @@
- #aws ec2 describe-instances   #Details of the instance
+ aws ec2 describe-instances   #Details of the instance
  #aws ec2 describe-subnets   #Details of the subnets
 # aws ec2 describe-instances --instance-id i-06de2e63e4b57f1a8\
 #   --query 'Reservations[*].Instances[*].[InstanceId,ImageId,Tags[*]]' \
@@ -8,9 +8,9 @@
 #  #Details of InstanceId and Name of stopped instances
  #aws ec2 describe-instances --filters 'Name=instance-state-name,Values=stopped' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`].Value | [0],InstanceId:InstanceId}' --output table
 #  #Details of ram
- aws ec2 describe-instances --instance-ids <instance-id> --query 'Reservations[].Instances[].MemoryInfo.SizeInMiB' --output text
+ #aws ec2 describe-instances --instance-ids <instance-id> --query 'Reservations[].Instances[].MemoryInfo.SizeInMiB' --output text
 # #AZ Details
- aws ec2 describe-instances --instance-ids i-06de2e63e4b57f1a8 --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,Placement.AvailabilityZone]' --output text
+ #aws ec2 describe-instances --instance-ids i-06de2e63e4b57f1a8 --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,Placement.AvailabilityZone]' --output text
 # #Instance Details running or stopped 
 #  aws ec2 describe-instances --filters Name=instance-state-name,Values=running --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PrivateIpAddress,PublicIpAddress,State.Name,Placement.AvailabilityZone]"
 # #Running processes
