dac58c9d9fdfea95c155d03d0d8d4362bec91907
Shoaibnadeem <nadeem.naddu7@gmail.com>
2023-03-19 19:18:44 +0530

    initial commit

--- report.sh
+++ report.sh
@@ -9,7 +9,7 @@
 #  #Details of InstanceId and Name of stopped instances
  aws ec2 describe-instances --filters 'Name=instance-state-name,Values=stopped' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`].Value | [0],InstanceId:InstanceId}' --output table
 #  #Details of ram
- aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[*].Instances[*].MemoryInfo.SizeInMiB' --output text
+ aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[*].Instances[*].[InstanceType, MemoryInfo.SizeInMiB, MemoryInfo.Unit]' --output text
 # #AZ Details
  aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,Placement.AvailabilityZone]' --output text
 # #Instance Details running or stopped 
