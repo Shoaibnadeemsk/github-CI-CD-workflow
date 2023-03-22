81f3fb5c0602f89ec20bc3b71c6703ffa27a98f5
Shoaibnadeem <nadeem.naddu7@gmail.com>
2023-03-23 01:11:42 +0530

    initial commit

--- report.sh
+++ report.sh
@@ -13,9 +13,9 @@
 # #AZ Details
  aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,Placement.AvailabilityZone]' --output text
 # #Instance Details running or stopped 
- #aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PrivateIpAddress,PublicIpAddress,State.Name,Placement.AvailabilityZone,RamdiskId]"
+ aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PrivateIpAddress,PublicIpAddress,State.Name,Placement.AvailabilityZone,RamdiskId]"
 # #Running processes
-# aws ec2 describe-instances --instance-ids i-06de2e63e4b57f1a8 --query "Reservations[*].Instances[*].RunningProcesses[].ProcessName" --output text | wc -l
+aws ec2 describe-instances --instance-ids $Instance_Id --query "Reservations[*].Instances[*].RunningProcesses[].ProcessName" --output text 
 # #Details of the AZ, instances, and name
 # aws ec2 describe-instances \
 #     --filters Name=tag-key,Values=Name \
