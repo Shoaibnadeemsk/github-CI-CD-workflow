4d1d3cd73a83aaa3971071f9853c2920115a316a
Shoaibnadeem <nadeem.naddu7@gmail.com>
2023-03-23 00:37:37 +0530

    initial commit

--- report.sh
+++ report.sh
@@ -1,19 +1,19 @@
  aws ec2 describe-instances   #Details of the instance
- aws ec2 describe-subnets   #Details of the subnets
- aws ec2 describe-instances --instance-id $Instance_Id\
-   --query 'Reservations[*].Instances[*].[InstanceId,ImageId,Tags[*]]' \
-   --output text         # Details of instanceId, ImageId and Tags
+ #aws ec2 describe-subnets   #Details of the subnets
+#  aws ec2 describe-instances --instance-id $Instance_Id\
+#    --query 'Reservations[*].Instances[*].[InstanceId,ImageId,Tags[*]]' \
+#    --output text         # Details of instanceId, ImageId and Tags
  
  #Details of InstanceId and Name of running instances
- aws ec2 describe-instances --filters 'Name=instance-state-name,Values=running' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`].Value | [0],InstanceId:InstanceId}' --output table 
+ #aws ec2 describe-instances --filters 'Name=instance-state-name,Values=running' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`].Value | [0],InstanceId:InstanceId}' --output table 
 #  #Details of InstanceId and Name of stopped instances
- aws ec2 describe-instances --filters 'Name=instance-state-name,Values=stopped' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`].Value | [0],InstanceId:InstanceId}' --output table
+ #aws ec2 describe-instances --filters 'Name=instance-state-name,Values=stopped' --query 'Reservations[].Instances[].{Name:Tags[?Key==`Name`].Value | [0],InstanceId:InstanceId}' --output table
 #  #Details of ram
- aws ec2 describe-instances --instance-ids i-06474f0e051d3c623 --query 'Reservations[*].Instances[*].[InstanceType, MemoryInfo.SizeInMiB, MemoryInfo.Unit]' --output text
+ #aws ec2 describe-instances --instance-ids i-06474f0e051d3c623 --query 'Reservations[*].Instances[*].[InstanceType, MemoryInfo.SizeInMiB, MemoryInfo.Unit]' --output text
 # #AZ Details
- aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,Placement.AvailabilityZone]' --output text
+ #aws ec2 describe-instances --instance-ids $Instance_Id --query 'Reservations[*].Instances[*].[InstanceId,InstanceType,Placement.AvailabilityZone]' --output text
 # #Instance Details running or stopped 
- aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PrivateIpAddress,PublicIpAddress,State.Name,Placement.AvailabilityZone,RamdiskId]"
+ #aws ec2 describe-instances --filters Name=instance-state-name,Values=stopped --query "Reservations[*].Instances[*].[InstanceId,InstanceType,PrivateIpAddress,PublicIpAddress,State.Name,Placement.AvailabilityZone,RamdiskId]"
 # #Running processes
 # aws ec2 describe-instances --instance-ids i-06de2e63e4b57f1a8 --query "Reservations[*].Instances[*].RunningProcesses[].ProcessName" --output text | wc -l
 # #Details of the AZ, instances, and name
@@ -79,4 +79,4 @@
 #aws ssm get-inventory --instance-id <instance-id> --query "Entities[*].Data.Applications[?Name=='httpd'].Version | [0]" --output text
 #aws ssm describe-instance-information --instance-information-filter-list "key=PingStatus,value=Online" --query "InstanceInformationList[0].InstanceId" --output text | xargs aws ssm list-inventory-entries --instance-id | jq '.Entries[] | select(.TypeName == "AWS:Application") | {Name: .Name, Version: .Version}'
 
-ssh -i ./GA_Example.pem ec2-user@3.110.184.42
+#ssh -i ./GA_Example.pem ec2-user@3.110.184.42
