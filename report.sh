de8da2678f70aa6eb0600bb5e32f383c738dfc46
Shoaibnadeem <nadeem.naddu7@gmail.com>
2023-03-23 22:30:17 +0530

    initial commit

--- report.sh
+++ report.sh
@@ -35,6 +35,7 @@ aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name MemoryUti
  --start-time 2023-03-18T23:18:00 --end-time 2023-03-24T23:22:00
 
 free -h
+aws ec2 describe-instances --instance-ids $instance --query 'Reservations[].Instances[] | {InstanceId: InstanceId, PublicIpAddress: PublicIpAddress, SubnetId: SubnetId, InstanceType: InstanceType}'
 # aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name MemoryUsage  --period 3600 \
 #  --statistics Maximum --dimensions Name=InstanceId,Value=i-06474f0e051d3c623 \
 #  --start-time 2023-03-18T23:18:00 --end-time 2023-03-24T23:22:00
