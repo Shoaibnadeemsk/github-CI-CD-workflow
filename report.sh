b23c82e76dbd553ccfa22293e9cc2bf090900d76
Shoaibnadeem <nadeem.naddu7@gmail.com>
2023-03-23 19:22:21 +0530

    initial commit

--- report.sh
+++ report.sh
@@ -35,9 +35,10 @@ aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name MemoryUti
  --start-time 2023-03-18T23:18:00 --end-time 2023-03-24T23:22:00
 
 free -h
-aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name MemoryUsage  --period 3600 \
- --statistics Maximum --dimensions Name=InstanceId,Value=i-06474f0e051d3c623 \
- --start-time 2023-03-18T23:18:00 --end-time 2023-03-24T23:22:00
+# aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name MemoryUsage  --period 3600 \
+#  --statistics Maximum --dimensions Name=InstanceId,Value=i-06474f0e051d3c623 \
+#  --start-time 2023-03-18T23:18:00 --end-time 2023-03-24T23:22:00
+ 
 
 
 # #to list all installed packages on Ubuntu
