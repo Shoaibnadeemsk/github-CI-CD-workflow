7934ab01bc9a369a3104242c68602b8c2fc04bf1
Shoaibnadeem <nadeem.naddu7@gmail.com>
2023-03-24 14:45:21 +0530

    initial commit

--- report.sh
+++ report.sh
@@ -32,6 +32,10 @@
 
 free -h
  aws ec2 stop-instances --instance-ids i-06f53edb075a0edc2  # Stopped the instance
+  aws ec2 describe-instances \
+ --instance-ids  i-06f53edb075a0edc2 \
+ --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name,InstanceID:InstanceId,Instancetype:InstanceType}"  \
+ --output table     #Validated whether it is stopped or not 
 
 
 #aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name CPUUtilization  --period 3600 --statistics Maximum --dimensions Name=InstanceId,Value="$Instance_Id"  --start-time 2022-10-18T23:18:00 --end-time 2022-10-19T23:18:00
@@ -70,13 +74,9 @@ free -h
 
 #aws ec2 stop-instances --instance-ids i-06f53edb075a0edc2
 
- aws ec2 describe-instances \
- --instance-ids  i-06f53edb075a0edc2 \
- --query "Reservations[*].Instances[*].{PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name,InstanceID:InstanceId,Instancetype:InstanceType}"  \
- --output table
 
-#  aws ec2 modify-instance-attribute \
-#  --instance-id i-0ea05822320404483 \
-#  --instance-type "{\"Value\": \"t2.small\"}"
+  aws ec2 modify-instance-attribute \
+  --instance-id i-06f53edb075a0edc2 \
+  --instance-type "{\"Value\": \"t2.small\"}"
  
  #aws ec2 start-instances --instance-ids i-0ea05822320404483
